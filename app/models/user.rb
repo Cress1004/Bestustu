e49e6db847d5d180ad11f2fbfbaf61e55b34f68b
cress1004 <nuhoangchenhac@gmail.com>
2020-03-05 15:18:21 +0700

    skip facebook omniauth confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,7 @@ class User < ApplicationRecord
        user.password = Devise.friendly_token[0,20]
        user.name = auth.info.name   # assuming the user model has a name
        user.image = auth.info.image # assuming the user model has an image
+       user.skip_confirmation!
      end
    end
 end
