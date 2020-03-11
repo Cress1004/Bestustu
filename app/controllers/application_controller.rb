ee685c77662af017089b0e498c774ab80565b38c
cress1004 <nuhoangchenhac@gmail.com>
2020-03-11 14:49:04 +0700

    add edit and show tutor information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,25 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   # Config for username authen
   protect_from_forgery with: :exception
-
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
+    end
+  end
 
   protected
-
   def configure_permitted_parameters
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me, :image, :phone_number,
                     :gender, :name, :address]
