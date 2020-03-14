790b799992c6ebb5fcc5d65c1fb7c8e236a8b22a
cress1004 <nuhoangchenhac@gmail.com>
2020-03-14 21:04:30 +0700

    last merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_03_12_031235) do
 
   create_table "class_registers", force: :cascade do |t|
