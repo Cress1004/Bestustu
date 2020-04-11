b68af16b6d16fed2086700efc634555a6c5e4829
cress1004 <nuhoangchenhac@gmail.com>
2020-04-11 14:59:53 +0700

    ..

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,14 +53,15 @@ Rails.application.configure do
 
      # ActionMailer Config
     config.action_mailer.default_url_options = { :host => 'https://bestustu.herokuapp.com/'}
+    config.action_mailer.perform_deliveries = true
     ActionMailer::Base.delivery_method = :smtp
     ActionMailer::Base.smtp_settings = {
-     address: ‘smtp.sendgrid.net’,
-     port: “25”,
-     domain: ‘heroku.com’,
+     address: "smtp.gmail.com",
+     port: "587",
+     domain: "heroku.com",
      user_name: "bestustuvn@gmail.com",
      password: "huythanhloc",
-     authentication: ‘plain’,
+     authentication: "plain",
      enable_starttls_auto: true
     }
 
