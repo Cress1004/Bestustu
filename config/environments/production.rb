6550a2ed22dde8ce95e49477b54d32d6a7d2cba4
cress1004 <nuhoangchenhac@gmail.com>
2020-04-11 20:38:05 +0700

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,13 +86,6 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-   # ActionMailer Config
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
