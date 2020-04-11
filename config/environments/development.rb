7cc0c8b9bcade2c4d130b88697facdf8142cd2ab
cress1004 <nuhoangchenhac@gmail.com>
2020-04-11 16:20:29 +0700

    ..

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     config.action_mailer.default_url_options = { :host => 'localhost:3000' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
-
+    #https://www.bogotobogo.com/RubyOnRails/RubyOnRails_Devise_Authentication_Sending_Confirmation_Email_Heroku_Deploy.php
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
